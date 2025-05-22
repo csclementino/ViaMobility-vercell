faf5f71648389303ff81462520b308d2ef177c10
Carlos Clementino <carlossantiagoclementino36@gmail.com>
2025-05-22 13:49:45 -0300

    Update eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -16,6 +16,7 @@ const eslintConfig = [
       "@typescript-eslint/no-unused-vars": "warn", // muda para aviso
       "react/no-unescaped-entities": "off",
       "@next/next/no-page-custom-font": "off",
+      "@typescript-eslint/no-explicit-any": "off",
     },
   }),
 ];
