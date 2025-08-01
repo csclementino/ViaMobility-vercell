e7d8887332eb3c80ce1523e266eeb79b9e849daa
Carlos Clementino <carlossantiagoclementino36@gmail.com>
2025-08-01 13:42:49 -0300

    Update page.tsx

--- src/components/botaoIcone/page.tsx
+++ src/components/botaoIcone/page.tsx
@@ -2,25 +2,24 @@ import React from 'react';
 
 interface BotaoIconeProps {
   children: React.ReactNode;
-  icone: string;
+  icone: React.ReactNode;
   onClick?: () => void;
 }
 
 export const BotaoIcone = ({ children, icone, onClick }: BotaoIconeProps) => {
   return (
     <button 
-      className=" flex flex-col items-center h-[19vh] w-[11vh] border-2 border-white rounded-[25px] cursor-pointer transition-colors duration-200 hover:bg-[#616161]"
+      className=" flex flex-col items-center h-[14vh] w-[92%] bg-[#474747] rounded-[15px] cursor-pointer transition-colors duration-200 hover:bg-[#616161] px-[1vh]"
       onClick={onClick}
     >
-      <span className="flex flex-col gap-2 items-center h-full w-full justify-center">
-        <img 
-          src={icone} 
-          alt="Ícone" 
-          className="w-[7vh] h-[7vh] object-contain"
-        />
-        <span className="text-inherit text-[2vh] font-xxgeom leading-[100%]">{children}</span>
+      <span className="flex flex-col gap-2 items-start h-full w-full justify-center">
+        <span className="w-[7vh] h-[7vh] flex items-center justify-start">
+          {icone} 
+        </span>
+        <span className="text-inherit text-[2vh] font-xxgeom leading-[100%] text-left">
+          {children}
+        </span>
       </span>
-      
       
     </button>
     
